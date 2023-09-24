4af159e0fd1410871b3e4038ccb49a76be482fd3
nicolasea17 <99514586+nicolasea17@users.noreply.github.com>
2023-09-24 18:59:50 +0300

    Update 325.py

--- 325.py
+++ 325.py
@@ -1,61 +1,48 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Load earthquake data
 df = pd.read_csv("earthquake_data.csv")
 
-# Plotting earthquake locations
+# Plotting earthquake locations using Matplotlib
 st.write("Earthquake Locations")
-fig_map = px.scatter_geo(df, lat='latitude', lon='longitude', color='magnitude', hover_name='location',
-                         title='Earthquake Locations')
-st.plotly_chart(fig_map)
+plt.scatter(df['longitude'], df['latitude'], c=df['magnitude'], cmap='viridis', alpha=0.7)
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Earthquake Locations')
+st.pyplot()
 
-# Magnitude Distribution Over Time (Animated Histogram)
+# Magnitude Distribution Over Time (Animated Histogram) using Matplotlib
 st.write("Magnitude Distribution Over Time")
-fig_histogram = px.histogram(df, x='magnitude', nbins=30, title='Magnitude Distribution Over Time')
-st.plotly_chart(fig_histogram)
+plt.hist(df['magnitude'], bins=30, alpha=0.7, color='blue')
+plt.xlabel('Magnitude')
+plt.ylabel('Frequency')
+plt.title('Magnitude Distribution Over Time')
+st.pyplot()
 
-# 3D Scatter Plot
+# 3D Scatter Plot using Matplotlib
 st.write("3D Scatter Plot of Earthquake Data (Depth, Magnitude, Gap)")
-fig_3d_scatter = px.scatter_3d(df, x='depth', y='magnitude', z='gap', color='country',
-                                size='nst', opacity=0.7, hover_name='title',
-                                labels={'depth': 'Depth', 'magnitude': 'Magnitude', 'gap': 'Gap',
-                                        'country': 'Country', 'nst': 'NST'},
-                                title='3D Scatter Plot of Earthquake Data (Depth, Magnitude, Gap)')
-st.plotly_chart(fig_3d_scatter)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(df['depth'], df['magnitude'], df['gap'], c=df['country'], s=df['nst'], alpha=0.7)
+ax.set_xlabel('Depth')
+ax.set_ylabel('Magnitude')
+ax.set_zlabel('Gap')
+ax.set_title('3D Scatter Plot of Earthquake Data (Depth, Magnitude, Gap)')
+st.pyplot()
 
-# Earthquake Density Contours by Magnitude
-st.write("Earthquake Density Contours by Magnitude")
-fig_density_contour = px.density_contour(df, x='longitude', y='latitude', color='magnitude',
-                                        marginal_x='histogram', marginal_y='histogram',
-                                        labels={'longitude': 'Longitude', 'latitude': 'Latitude', 'magnitude': 'Magnitude'},
-                                        title='Earthquake Density Contours by Magnitude')
-st.plotly_chart(fig_density_contour)
-
-# Cumulative Earthquake Count Over Time (Animated Line Chart)
+# Cumulative Earthquake Count Over Time (Animated Line Chart) using Matplotlib
 st.write("Cumulative Earthquake Count Over Time")
 df['date_time'] = pd.to_datetime(df['date_time'])
 df.sort_values(by='date_time', inplace=True)
 df['cumulative_count'] = range(1, len(df) + 1)
 
-fig_cumulative_count = go.Figure(
-    data=[
-        go.Scatter(
-            x=df['date_time'],
-            y=df['cumulative_count'],
-            mode='lines+markers',
-            line=dict(color='blue', width=2),
-            marker=dict(size=6, color='red'),
-            name='Cumulative Count'
-        ),
-    ],
-    layout=go.Layout(
-        title='Cumulative Earthquake Count Over Time',
-        xaxis=dict(title='Date'),
-        yaxis=dict(title='Cumulative Count'),
-    ),
-)
-
-st.plotly_chart(fig_cumulative_count)
+plt.figure()
+plt.plot(df['date_time'], df['cumulative_count'], color='blue', marker='o')
+plt.xlabel('Date')
+plt.ylabel('Cumulative Count')
+plt.title('Cumulative Earthquake Count Over Time')
+plt.xticks(rotation=45)
+st.pyplot()
