db8fdfb7a165eb29290741d2ff1dd492f370a382
nicolasea17 <99514586+nicolasea17@users.noreply.github.com>
2023-09-24 18:28:53 +0300

    Update 325.py

--- 325.py
+++ 325.py
@@ -1,7 +1,10 @@
+import os
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 
 # Load earthquake data from the CSV file in the same directory
-df = pd.read_csv('earthquake.csv')
+script_directory = os.path.dirname(os.path.abspath(__file__))
+csv_file_path = os.path.join(script_directory, 'earthquake.csv')
+df = pd.read_csv(csv_file_path)
